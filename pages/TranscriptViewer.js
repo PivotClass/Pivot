944d91cec6388ce2d94a064412cdcce2727f2262
Jiahua Chen <jc@jh.tc>
2020-10-04 17:36:50 +0800

    TranscriptViewer Component

---
+++ pages/TranscriptViewer.js
@@ -0,0 +1,43 @@
+import React from 'react' ;
+import IconButton from '@material-ui/core/IconButton';
+import Button from '@material-ui/core/Button';
+import MicIcon from '@material-ui/icons/Mic';
+import MicOffIcon from '@material-ui/icons/MicOff';
+import Typography from '@material-ui/core/Typography';
+import Fab from '@material-ui/core/Fab';
+
+
+export default function TranscriptViewer(props) {
+
+    return (
+        <div
+            className="transcript_view"
+            style={{
+                position: "absolute",
+                top: "0",
+                bottom: "0",
+                left: "0",
+                right: "0",
+            }}
+        >
+            <Typography
+                variant="h4"
+                style={{
+                    position: "absolute",
+                    left: "40px",
+                    right: "40px",
+                    top: "40px",
+                    bottom: "40px",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    textAlign: "center",
+                    verticalAlign: "middle",
+                }}
+                gutterBottom>
+                // TODO: change this.state.current_output to transcript from teacher.
+                {this.state.current_output ? this.state.current_output : "(Transcription hasn't started yet)"}
+            </Typography>
+        </div>
+    )
+}
